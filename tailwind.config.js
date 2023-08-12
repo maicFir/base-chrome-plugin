a3665fbd0459aa9c5527559fa38b5901d62c8018
maicFir <mcodes@163.com>
2023-08-12 22:08:56 +0800

    add scss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./src/**/*.{js,ts,jsx,tsx}"],
-    darkMode: ["class", '[data-theme="dark"]'],
-    theme: {
-      extend: {
-        colors: {
-          "theme-text-color": "var(--bubble-ai-theme-text-color)",
-        },
-        screens: {
-         
-        },
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: ["class", '[data-theme="dark"]'],
+  theme: {
+    extend: {
+      colors: {
+        "theme-text-color": "var(--theme-text-color)",
       },
+      screens: {},
     },
-    plugins: [],
-  };
-  
+  },
+  plugins: [],
+};
